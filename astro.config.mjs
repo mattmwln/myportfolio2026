d467e2bbb0a198a8f5d23b67ebfbd47ad48b0df3
mattmwln <rahmatmaulanapersonal@gmail.com>
2026-03-29 06:01:09 +0700

    Update tampilan About section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static"; // pakai static adapter
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
+  output: "static", // ganti dari server
   adapter: vercel()
 });
